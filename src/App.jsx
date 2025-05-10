7a7da55aa942c32ba3391c997355c39584cc346c
hahafiz <hafizqamarul19@gmail.com>
2025-05-10 20:06:48 +0800

    customization and typography

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,18 @@ function App() {
 
   return (
     <>
-      <h1 class="bg-miwulmajoo text-white border-4 border-amber-400">
+      <h1 class="bg-miwulmajoo text-white border-4 border-amber-400 text-sm lg:text-lg">
         Hello world!!!!
       </h1>
       <h1 class="text-miwulmajoo">Hello world!!!!</h1>
+      <br />
+      <h1>Typography sandbox</h1>
+      <h1 class="text-9xl">Title 1</h1>
+      <h1 class="text-5xl">Title 2</h1>
+      <h1 class="text-xl">Title 3</h1>
+      <p class="text-base">A regular paragraph</p>
+      <p class="text-sm">A description paragraph</p>
+      <p class="text-xs note">A little footnote</p>
     </>
   );
 }
