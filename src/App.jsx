d2c2599b18a37e6c8a91c8eeca2c05ea38130ed9
hahafiz <hafizqamarul19@gmail.com>
2025-05-16 22:01:40 +0800

    covered flex. 1:51:48

--- src/App.jsx
+++ src/App.jsx
@@ -57,6 +57,60 @@ function App() {
         <div className="w-2/5 h-16 bg-blue-500 min-w-min"></div>
         <div className="w-3/5 h-16 bg-green-500"></div>
       </div>
+      <br />
+      <h1 className="text-4xl text-bold">Flex</h1>
+      <div className="parent">
+        <div>Header</div>
+        <div className="flex">
+          <div className="grow">Main Content</div>
+          <div className="flex">
+            <div className="basis-3/4">Sidebar</div>
+            <div className="basis-1/4">Menu</div>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div className="flex flex-col">
+        <div>Header</div>
+        <div className="flex flex-col md:flex-row">
+          <div className="w-32 bg-sky-500">Sidebar</div>
+          <div className="w-full bg-red-600">Main Content</div>
+        </div>
+      </div>
+      <br />
+      <div className="flex flex-col md:flex-row">
+        <div className="flex w-full flex-wrap">
+          <div className="w-32 grow">01</div>
+          <div className="w-32 grow">02</div>
+          <div className="w-32 grow">03</div>
+          <div className="w-32 grow">04</div>
+          <div className="w-32 grow">05</div>
+        </div>
+      </div>
+      <br />
+      <div className="flex justify-evenly">
+        <div>Justify</div>
+        <div>Main Content</div>
+        <div>Main Content</div>
+      </div>
+      <br />
+      <div className="flex justify-center items-center">
+        <div>Align</div>
+        <div>
+          <h1>Random title</h1>
+          <p>
+            lorem ipsum dolor sit amet consectetur adipiscing elit. Fuga, quis ?
+            Exercitationem amet maxime
+          </p>
+        </div>
+        <div>Main Content</div>
+      </div>
+      <br />
+      <div className="flex items-baseline">
+        <div className="pt-2 pb-6 bg-sky-500">01</div>
+        <div className="pt-8 pb-12 bg-red-500">02</div>
+        <div className="pt-12 pb-4 bg-green-500">02</div>
+      </div>
     </>
   );
 }
